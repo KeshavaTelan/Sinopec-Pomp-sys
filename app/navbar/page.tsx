4dd89693acdee595c09bc043d193f94d38281e95
Keshava.Silva <keshava.silva@outlook.com>
2024-04-09 13:15:23 +0530

    project started

---
+++ app/navbar/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const NavBar = (props: Props) => {
+  return (
+   <div>
+         https://www.youtube.com/watch?v=ZVnjOPwW4ZA
+<br></br>
+         <button className="btn btn-primary">Primary</button>
+   </div>
+  )
+}
+
+export default NavBar ;
