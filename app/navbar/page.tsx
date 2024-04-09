ea30539e7e152b5f521359b27f5162b88a06a954
Keshava.Silva <keshava.silva@outlook.com>
2024-04-09 14:41:15 +0530

    nav drawer

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -4,11 +4,22 @@ type Props = {}
 
 const NavBar = (props: Props) => {
   return (
-   <div>
-         https://www.youtube.com/watch?v=ZVnjOPwW4ZA
-<br></br>
-         <button className="btn btn-primary">Primary</button>
-   </div>
+<div className="drawer">
+  <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+    {/* Page content here */}
+    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+      {/* Sidebar content here */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+      
+    </ul>
+  </div>
+</div>
   )
 }
 
